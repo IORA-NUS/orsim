fae0efb8d615d6cbdd7e6c8c2511f7ae54836ae3
Rajiv <rajeevamoorthy@users.noreply.github.com>
2022-01-12 16:52:55 +0800

    updated dist

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
+import pathlib
 from setuptools import find_packages, setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
 
 setup(
     name='orsim',
@@ -6,8 +14,16 @@ setup(
     packages=find_packages(),
     version='0.1.0',
     description='Distributed Agent based Simulation Library',
+    url='https://pypi.org/project/orsim/0.1.0/',
+    long_description=README,
+    long_description_content_type="text/markdown",
     author='iora_dev_team',
     license='MIT',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
     install_requires=[],
     setup_requires=['pytest-runner'],
     tests_require=['pytest>=4.4.1'],
