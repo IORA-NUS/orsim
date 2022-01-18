e4c5b7258152eae4ae109165953699507b9bf00e
Rajiv <rajeevamoorthy@users.noreply.github.com>
2022-01-18 16:58:32 +0800

    Library includes task and Celery process executor

--- orsim/tasks/agent_task.py
+++ orsim/tasks/agent_task.py
@@ -1,7 +1,8 @@
 
-
-
 from __future__ import absolute_import
+
+import sys
+from orsim.core import ORSimEnv
 # from apps.worker import app
 from orsim.worker import app
 
@@ -9,14 +10,17 @@ from orsim.worker import app
 from celery.signals import after_setup_task_logger
 
 @app.task
-def start_agent(module_path, agent_class_name, **kwargs):
+def start_agent(project_path, module_name, agent_class_name, messenger_settings, **kwargs):
 
-    # from orsim import ORSimEnv
+    if not project_path in sys.path:
+        sys.path.append(project_path)
+    print(sys.path)
     # from apps.config import messenger_backend
-    # ORSimEnv.set_backend(messenger_backend)
+
+    ORSimEnv.set_backend(messenger_settings)
 
     import importlib
-    module = importlib.import_module(module_path)
+    module = importlib.import_module(module_name)
     AgentClass = getattr(module, agent_class_name)
 
     agent = AgentClass(**kwargs)
--- orsim/worker.py
+++ orsim/worker.py
@@ -1,29 +1,12 @@
 
 from __future__ import absolute_import
 from celery import Celery
-# from .config import settings
-# # import logging
 
-# app = Celery('apps',
-#              broker=f'amqp://{settings["RABBITMQ_ADMIN_USER"]}:{settings["RABBITMQ_ADMIN_PASSWORD"]}@{settings["MQTT_BROKER"]}',
-#              backend='rpc://',
-#              include=['apps.tasks'])
 
-from orsim.core import ORSimEnv
+from .celery_config import CeleryConfig
 
-broker_url = f'amqp://{ORSimEnv.messenger_backend["RABBITMQ_ADMIN_USER"]}:{ORSimEnv.messenger_backend["RABBITMQ_ADMIN_PASSWORD"]}@{ORSimEnv.messenger_backend["MQTT_BROKER"]}'
-
-## Disable result backent and also ignore results.
-task_ignore_result = True
-
-# List of modules to import when the Celery worker starts.
-imports = ('orsim.tasks',)
-
-app = Celery('OpenRoad_RideHail_Agents',
-            broker=broker_url,
-            task_ignore_result=True,
-            include=imports)
-
+app = Celery('OpenRoad_Agents',
+             config_from_object=CeleryConfig)
 
 if __name__ == "__main__":
     app.start()
